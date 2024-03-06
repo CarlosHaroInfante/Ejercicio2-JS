c676da9fd02d33693c4ae09e64f52ea0e7e81b1b
CarlosHaroInfante <145343573+CarlosHaroInfante@users.noreply.github.com>
2024-03-06 19:26:00 +0100

    Add files via upload

---
+++ Ejercicio2Java/src/main/webapp/js.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+
+
+ var fechaHoy = {
+	 
+	 dia: 6,
+	 mes: 3,
+	 anyo: 2024
+	 }
+	 
+ var calendario = {
+ 	
+ 	dia: 6,
+ 	mes: "Marzo",
+ 	anyo: 2024
+	}
+	 
+	 if(fechaHoy == calendario){
+		 
+		 document.write("Es Hoy");
+		 
+	 }
+	 
+	 else{
+		document.write("Aún falta"); 
+	 }
+	 
+	 
+ 
